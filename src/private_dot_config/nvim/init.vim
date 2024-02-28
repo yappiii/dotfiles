3e449cde51b19ab7823315ecb588253adeb7cddb
yappiii <rice410shopper@gmail.com>
2024-02-28 19:31:27 +0900

    refactor: :recycle: vim script to lua

--- src/private_dot_config/nvim/init.vim
+++ src/private_dot_config/nvim/init.vim
@@ -1,31 +1,6 @@
-if &compatible
-	set nocompatible
-endif
-set runtimepath+=~/.cache/dein/repos/github.com/Shougo/dein.vim
-if dein#load_state('~/.cache/dein')
-	call dein#begin('~/.cache/dein')
-	call dein#load_toml('~/.config/nvim/dein.toml', {'lazy': 0})
-	call dein#end()
-	call dein#save_state()
-endif
-
-" 不足プラグインの自動インストール
-if has('vim_starting') && dein#check_install()
-	call dein#install()
-endif
-
 lua require('anyenv')
 lua require('core')
 lua require('plugins')
-
-" copilotをluaに移行してからこちらも移行する
-let g:copilot#enable = 1
-let g:copilot#enable_at_startup = 1
-let g:copilot#no_tab_maps = 1
-
-""" bufferを指定して移動
-nnoremap <silent> <space>bn :<C-u>bnext<CR>
-nnoremap <silent> <space>bp :<C-u>bprev<CR>
 
 filetype plugin indent on
 syntax enable
--- src/private_dot_config/nvim/lua/plugins/config/lspconfig.lua
+++ src/private_dot_config/nvim/lua/plugins/config/lspconfig.lua
@@ -1,9 +1,26 @@
+local on_attach = function(client, bufnr)
+  vim.api.nvim_set_keymap("n", "gd", vim.lsp.buf.definition, {
+    noremap = true,
+    silent = true
+  })
+  vim.api.nvim_set_keymap("n", "gD", vim.lsp.buf.declaration, {
+    noremap = true,
+    silent = true
+  })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = buffer,
+    callback = function()
+        vim.lsp.buf.format { async = false }
+    end
+  })
+end
+
 require("lspconfig").gopls.setup({
   cmd = {
     "gopls",
     "serve"
   },
-  on_attach = require("lsp").common_on_attach,
+  on_attach = on_attach,
   settings = {
     gopls = {
       analyses = {
@@ -12,19 +29,4 @@ require("lspconfig").gopls.setup({
       staticcheck = true
     }
   }
-})
-
-vim.api.nvim_set_keymap("n", "gd", vim.lsp.buf.definition, {
-  noremap = true,
-  silent = true
-})
-vim.api.nvim_set_keymap("n", "gD", vim.lsp.buf.declaration, {
-  noremap = true,
-  silent = true
-})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  buffer = buffer,
-  callback = function()
-      vim.lsp.buf.format { async = false }
-  end
 })
