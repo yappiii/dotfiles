9b4f84a324a6ad37c1fe14e9f1859832884b636b
yappiii <rice410shopper@gmail.com>
2024-03-01 16:28:31 +0900

    feat: :sparkles: add dap conf

--- src/private_dot_config/nvim/lua/plugins/config/dap.lua
+++ src/private_dot_config/nvim/lua/plugins/config/dap.lua
@@ -1,3 +1,58 @@
+require("dap").configurations.go = {
+    {
+      type = "go",
+      name = "Debug",
+      request = "launch",
+      program = "${file}",
+    },
+    {
+      type = "go",
+      name = "Debug test",
+      request = "launch",
+      mode = "test",
+      program = "${file}",
+    },
+    {
+      type = "go",
+      name = "Debug test (go.mod)",
+      request = "launch",
+      mode = "test",
+      program = "./${relativeFileDirname}",
+    }
+}
+require("dap").adapters.go = function(callback, config)
+    local stdout = vim.loop.new_pipe(false)
+    local handle
+    local pid_or_err
+    local port = 38697
+    local opts = {
+      stdio = {nil, stdout},
+      args = {"dap", "-l", "127.0.0.1:" .. port},
+      detached = true
+    }
+    handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+      stdout:close()
+      handle:close()
+      if code ~= 0 then
+        print('dlv exited with code', code)
+      end
+    end)
+    assert(handle, 'Error running dlv: ' .. tostring(pid_or_err))
+    stdout:read_start(function(err, chunk)
+      assert(not err, err)
+      if chunk then
+        vim.schedule(function()
+          require('dap.repl').append(chunk)
+        end)
+      end
+    end)
+    -- Wait for delve to start
+    vim.defer_fn(
+      function()
+        callback({type = "server", host = "127.0.0.1", port = port})
+      end,
+      100)
+end
 vim.api.nvim_set_keymap("n", "5", ":lua require('dap').continue()<CR>", { silent = true})
 vim.api.nvim_set_keymap("n", "6", ":lua require('dap').step_over()<CR>", { silent = true})
 vim.api.nvim_set_keymap("n", "7", ":lua require('dap').step_into()<CR>", { silent = true})
--- src/private_dot_config/nvim/lua/plugins/config/dapui.lua
+++ src/private_dot_config/nvim/lua/plugins/config/dapui.lua
@@ -1,1 +1,66 @@
+require("dapui").setup({
+	icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+	mappings = {
+		-- Use a table to apply multiple mappings
+		expand = { "<CR>", "<2-LeftMouse>" },
+		open = "o",
+		remove = "d",
+		edit = "e",
+		repl = "r",
+		toggle = "t",
+	},
+	-- Expand lines larger than the window
+	-- Requires >= 0.7
+	expand_lines = vim.fn.has("nvim-0.7") == 1,
+  layouts = {
+    {
+      elements = {
+        { id = "watches", size = 0.20 },
+        { id = "stacks", size = 0.20 },
+        { id = "breakpoints", size = 0.20 },
+        { id = "scopes", size = 0.40 },
+      },
+      size = 64,
+      position = "right",
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 0.20,
+      position = "bottom",
+    },
+  },
+	controls = {
+		-- Requires Neovim nightly (or 0.8 when released)
+		enabled = true,
+		-- Display controls in this element
+		element = "repl",
+		icons = {
+			pause = "",
+			play = "",
+			step_into = "",
+			step_over = "",
+			step_out = "",
+			step_back = "",
+			run_last = "↻",
+			terminate = "□",
+		},
+	},
+	floating = {
+		max_height = nil, -- These can be integers or a float between 0 and 1.
+		max_width = nil, -- Floats will be treated as percentage of your screen.
+		border = "single", -- Border style. Can be "single", "double" or "rounded"
+		mappings = {
+			close = { "q", "<Esc>" },
+		},
+	},
+	windows = { indent = 1 },
+	render = {
+		max_type_length = nil, -- Can be integer or nil.
+		max_value_lines = 100, -- Can be integer or nil.
+	}
+})
+
 vim.api.nvim_set_keymap("n", "<leader>d", ":lua require('dapui').toggle()<CR>", { noremap = true, silent = true })
